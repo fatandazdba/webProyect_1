0970546ab8db7ba9b3033fce0ee90f285a30d34a
fatandaz <fatandaz@gmail.com>
2021-04-20 17:37:31 +0200

    save User in DB - DBAL - select * from user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,14 +5,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\DBAL\Driver\Connection;
+
+use App\Entity\User;
 
 class UserController extends AbstractController
 {
     /**
      * @Route("/user", name="user")
      */
-    public function index(): Response
+    public function index(Connection $connection): Response
     {
+        $user = new User();
+        $entityManager = $this->getDoctrine()->getManager();
+        $user->setName('freddy');
+        $user->setLastName('tandazo');
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        $usersDB = $connection->fetchAll('SELECT * FROM user');
+
+        echo  print_r($usersDB, true);
+        //echo  print_r(phpinfo(), true);
+
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
