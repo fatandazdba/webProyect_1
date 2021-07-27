ddc496c4aa394d39784210aa55b0be7c9a438721
fatandaz <fatandaz@gmail.com>
2021-07-27 17:56:37 +0200

    consulta a la base de datos por medio de DBAL - \Doctrine\DBAL\DriverManager

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -24,8 +24,8 @@ class UserController extends AbstractController
         $entityManager->persist($user);
         $entityManager->flush();
 
+        //OTRA MANERA DE OBTENER LOS DATOS DESDE DB
         $usersDB = $connection->fetchAll('SELECT * FROM user');
-
         echo  print_r($usersDB, true);
         //echo  print_r(phpinfo(), true);
 
@@ -33,4 +33,40 @@ class UserController extends AbstractController
             'controller_name' => 'UserController',
         ]);
     }
+
+    /**
+     * @Route("/userDB", name="userDB")
+     */
+    public function userDB(Connection $connection): Response
+    {
+        //DB
+        $usersDB = $connection->fetchAll('SELECT * FROM user');
+        //echo "connection: \n <pre> " . var_dump($connection) ."</pre>". PHP_EOL;
+        echo  "UsersDB: ".PHP_EOL.print_r($usersDB, true).PHP_EOL;
+        //echo  print_r(phpinfo(), true);
+
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    /**
+     * @Route("/userDBAL", name="userDBAL")
+     */
+    public function userDBAL(Connection $connection): Response
+    {
+        //DBAL
+        $connectionParams = array('url' => $_ENV["DATABASE_URL"]);
+        $connectionDBAL = \Doctrine\DBAL\DriverManager::getConnection($connectionParams);
+        echo "connectionDBAL: \n <pre> " . print_r($connectionDBAL, true) ."</pre>". PHP_EOL;
+
+        $usersDBAL = $connectionDBAL->fetchAll('select * from user');
+        echo  "UsersDBAL: ".PHP_EOL.print_r($usersDBAL, true).PHP_EOL;
+        //echo  print_r(phpinfo(), true);
+
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
 }
