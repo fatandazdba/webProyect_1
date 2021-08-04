92cc23aa3093e0a43c154e037a34ff433628ad25
fatandaz <fatandaz@gmail.com>
2021-08-04 19:25:21 +0200

    php bin/console doctrine:mapping:import "App\Entity" annotation --path=src/Entity --filter="Payment"

---
+++ src/Entity/Payment.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Payment
+ *
+ * @ORM\Table(name="payment")
+ * @ORM\Entity
+ */
+class Payment
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var float|null
+     *
+     * @ORM\Column(name="quantity", type="float", precision=10, scale=0, nullable=true)
+     */
+    private $quantity;
+
+
+}
