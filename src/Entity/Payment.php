90975338db0be9237e6dbffeae68b5579cff84f3
fatandaz <fatandaz@gmail.com>
2021-08-04 19:29:23 +0200

    php bin/console make:entity --regenerate App\Entity\Payment - GENERAMOS LOS GETTER Y SETTER

--- src/Entity/Payment.php
+++ src/Entity/Payment.php
@@ -28,5 +28,22 @@ class Payment
      */
     private $quantity;
 
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuantity(): ?float
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(?float $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
 
 }
