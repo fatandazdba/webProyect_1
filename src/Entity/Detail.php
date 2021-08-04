ab14feb83cbefe0120fcea61573a45b762dde9b5
fatandaz <fatandaz@gmail.com>
2021-08-04 19:05:17 +0200

    php bin/console make:entity --regenerate App\Entity\Detail - GENERAMOS LOS GETTER Y SETTER

--- src/Entity/Detail.php
+++ src/Entity/Detail.php
@@ -28,5 +28,22 @@ class Detail
      */
     private $description;
 
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
 
 }
