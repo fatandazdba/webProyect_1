2425b17c1aae6059548e7cc6b1b87a415afb6d88
fatandaz <fatandaz@gmail.com>
2021-09-16 14:31:11 +0200

    formulario de registro de usuario - composer require symfony/twig-bridge

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\DBAL\Driver\Connection;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 
 use App\Entity\User;
 
@@ -80,6 +83,54 @@ class UserController extends AbstractController
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
+    }
+
+
+    /**
+     * @Route("/registroUsuario", name="registroUsuario")
+     */
+    public function registroUsuario(Request $request, JWTTokenManagerInterface $JWTManager)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //dd($request->request->get($form->getName()));
+            $email = $request->request->get($form->getName())["email"];
+            $password = $request->request->get($form->getName())["password"];
+            $name = $request->request->get($form->getName())["name"];
+            $lastName = $request->request->get($form->getName())["lastName"];
+            //$registro = $request->request->get($form->getName())["registro"];
+            //$token = $request->request->get($form->getName())["token"];
+
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $user->setEmail($email . date('dd:mm:yy hh:mm:ss'));
+            $user->setRoles(explode(",", $request->get('roles')));
+            $user->setPassword($this->userPasswordEncoderInterface->encodePassword(
+                $user,
+                $password
+            ));
+            $user->setName($name);
+            $user->setLastName($lastName);
+            $token = $this->getTokenUser($user, $JWTManager);
+            $user->setToken($token);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('registroUsuario');
+        }
+        return $this->render('user/registro.html.twig', ['controller_name' => 'Ingresar nuevo usuario', 'formulario' => $form->createView()]);
+    }
+
+    /**
+     * @param UserInterface $user
+     * @param JWTTokenManagerInterface $JWTManager
+     * @return JsonResponse
+     */
+    public function getTokenUser(User $user, JWTTokenManagerInterface $JWTManager)
+    {
+        return $JWTManager->create($user);
     }
 
 }
